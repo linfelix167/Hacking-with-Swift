a9c29dddb8942a00096f91fe34076697fdb2c75d
Felix <linfelix165@gmail.com>
2018-06-10 17:18:52 -0400

    Project 2 Init
    Project 2 Init

---
+++ Project 2 Guess the Flag/Project 2 Guess the Flag/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Project 2 Guess the Flag
+//
+//  Created by Felix Lin on 6/10/18.
+//  Copyright © 2018 Felix Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  @IBOutlet var button1: UIButton!
+  @IBOutlet var button2: UIButton!
+  @IBOutlet var button3: UIButton!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
