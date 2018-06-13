90ee5b75f4ff049d2b1fb34839361cf0651ad790
Felix <linfelix165@gmail.com>
2018-06-13 16:44:52 -0400

    Project 4 Init

---
+++ Project 4 Easy Browser/Project 4 Easy Browser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project 4 Easy Browser
+//
+//  Created by Felix Lin on 6/13/18.
+//  Copyright © 2018 Felix Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
