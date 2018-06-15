76f30e04f04ee32c6df2ec416396d7655ffdc895
Felix <linfelix165@gmail.com>
2018-06-14 20:18:07 -0400

    Add UIAlertController Actions

--- Project 4 Easy Browser/Project 4 Easy Browser/ViewController.swift
+++ Project 4 Easy Browser/Project 4 Easy Browser/ViewController.swift
@@ -25,13 +25,26 @@ class ViewController: UIViewController, WKNavigationDelegate {
     let url = URL(string: "https://hackingwithswift.com")!
     webView.load(URLRequest(url: url))
     webView.allowsBackForwardNavigationGestures = true
+    
+    navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
   }
   
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  @objc func openTapped() {
+    let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+    ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+    ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+    ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+    ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+    present(ac, animated: true)
   }
   
+  func openPage(action: UIAlertAction) {
+    let url = URL(string: "https://" + action.title!)!
+    webView.load(URLRequest(url: url))
+  }
   
+  func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+    title = webView.title
+  }
 }
 
