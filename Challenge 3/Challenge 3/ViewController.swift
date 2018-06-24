dee2c38f690f02d82955b38b9ad06b83f718eb89
Felix <linfelix165@gmail.com>
2018-06-24 15:35:52 -0400

    Challenge 3 Init

---
+++ Challenge 3/Challenge 3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Challenge 3
+//
+//  Created by Felix Lin on 6/24/18.
+//  Copyright © 2018 Felix Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
