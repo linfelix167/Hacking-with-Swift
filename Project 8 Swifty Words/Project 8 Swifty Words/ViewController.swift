9473fb439ab5d03bb160b6f9cb1a52c369595f1c
Felix <linfelix165@gmail.com>
2018-06-25 18:39:06 -0400

    Project 8 Init

---
+++ Project 8 Swifty Words/Project 8 Swifty Words/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project 8 Swifty Words
+//
+//  Created by Felix Lin on 6/25/18.
+//  Copyright © 2018 Felix Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
