f7540fe24e1a84f22b49768977798bae8d8101b3
Felix <linfelix165@gmail.com>
2018-06-25 12:35:15 -0400

    Challenge 3 finish

--- Challenge 3/Challenge 3/ViewController.swift
+++ Challenge 3/Challenge 3/ViewController.swift
@@ -9,12 +9,47 @@
 import UIKit
 
 class ViewController: UITableViewController {
-
+  
+  var shoppingList = [String]()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItem))
   }
-
-
+  
+  @objc func addItem() {
+    let ac = UIAlertController(title: "Add Item", message: nil, preferredStyle: .alert)
+    ac.addTextField()
+    let submitAction = UIAlertAction(title: "Add", style: .default) { [unowned self, ac] (action) in
+      let item = ac.textFields![0]
+      self.submit(item: item.text!)
+    }
+    ac.addAction(submitAction)
+    present(ac, animated: true)
+  }
+  
+  func submit(item: String) {
+    
+    if item.isEmpty == false {
+      shoppingList.insert(item, at: 0)
+      let indexPath = IndexPath(row: 0, section: 0)
+      tableView.insertRows(at: [indexPath], with: .automatic)
+    } else {
+      let ac = UIAlertController(title: "Empty", message: "Item is empty!", preferredStyle: .alert)
+      ac.addAction(UIAlertAction(title: "OK", style: .default))
+      present(ac, animated: true)
+    }
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return shoppingList.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "item", for: indexPath)
+    cell.textLabel?.text = shoppingList[indexPath.row]
+    return cell
+  }
 }
 
