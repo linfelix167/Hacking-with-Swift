6f39f6371a0ba9836956b506fe629904bb69aa81
Felix <linfelix165@gmail.com>
2018-06-27 11:57:11 -0400

    Challenge 4 Init

---
+++ Challenge 4/Challenge 4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Challenge 4
+//
+//  Created by Felix Lin on 6/27/18.
+//  Copyright © 2018 Felix Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
