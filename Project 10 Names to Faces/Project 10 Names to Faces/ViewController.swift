85a706a8b2ee4a142e4b621ff6c9adb0a86540fe
Felix <linfelix165@gmail.com>
2018-06-27 14:49:32 -0400

    Add CollectionView DataSource

--- Project 10 Names to Faces/Project 10 Names to Faces/ViewController.swift
+++ Project 10 Names to Faces/Project 10 Names to Faces/ViewController.swift
@@ -12,7 +12,16 @@ class ViewController: UICollectionViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+  }
+  
+  override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 10
+  }
+  
+  override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as! PersonCell
+    return cell
   }
 }
 
