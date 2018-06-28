f9cc212212444939f6530042da5edb96f13344c0
Felix <linfelix165@gmail.com>
2018-06-28 12:06:25 -0400

    UIImagePickerController

--- Project 10 Names to Faces/Project 10 Names to Faces/ViewController.swift
+++ Project 10 Names to Faces/Project 10 Names to Faces/ViewController.swift
@@ -8,11 +8,37 @@
 
 import UIKit
 
-class ViewController: UICollectionViewController {
+class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
+  }
+  
+  @objc func addNewPerson() {
+    let picker = UIImagePickerController()
+    picker.allowsEditing = true
+    picker.delegate = self
+    present(picker, animated: true)
+  }
+  
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    guard let image = info[UIImagePickerControllerEditedImage] as? UIImage else { return }
+    
+    let imageName = UUID().uuidString
+    let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+    
+    if let jpegData = UIImageJPEGRepresentation(image, 80) {
+      try? jpegData.write(to: imagePath)
+    }
+    dismiss(animated: true)
+  }
+  
+  func getDocumentsDirectory() -> URL {
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    let documentsDirectory = paths[0]
+    return documentsDirectory
   }
   
   override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
