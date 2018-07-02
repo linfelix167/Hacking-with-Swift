103fef937cd88fb3c27ad5e5d0e5e8973ee06df7
Felix <linfelix165@gmail.com>
2018-07-01 23:01:25 -0400

    Project 12 Init

---
+++ Project 12 UserDefaults/Project 12a/Project 10 Names to Faces/Person.swift
@@ -0,0 +1,21 @@
+//
+//  Person.swift
+//  Project 10 Names to Faces
+//
+//  Created by Felix Lin on 6/28/18.
+//  Copyright © 2018 Felix Lin. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+  
+  var name: String
+  var image: String
+  
+  init(name: String, image: String) {
+    self.name = name
+    self.image = image
+  }
+
+}
---
+++ Project 12 UserDefaults/Project 12a/Project 10 Names to Faces/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  Project 10 Names to Faces
+//
+//  Created by Felix Lin on 6/27/18.
+//  Copyright © 2018 Felix Lin. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var name: UILabel!
+  
+}
