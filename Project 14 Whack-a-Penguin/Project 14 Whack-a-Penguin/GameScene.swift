37b4b1e106e4c501e275820c966ca1c2222ff742
Felix <linfelix165@gmail.com>
2018-07-06 13:20:15 -0400

    Project 14 Init

---
+++ Project 14 Whack-a-Penguin/Project 14 Whack-a-Penguin/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  Project 14 Whack-a-Penguin
+//
+//  Created by Felix Lin on 7/6/18.
+//  Copyright © 2018 Felix Lin. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+  
+  override func didMove(to view: SKView) {
+  }
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+  }
+}
