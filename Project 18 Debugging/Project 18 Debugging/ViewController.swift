6e357a296243e093663d82f3cfa9074016423c63
Felix <linfelix165@gmail.com>
2018-07-12 14:51:21 -0400

    Add Project 18

---
+++ Project 18 Debugging/Project 18 Debugging/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Project 18 Debugging
+//
+//  Created by Felix Lin on 7/12/18.
+//  Copyright © 2018 Felix Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    for i in 1...100 {
+      print("Got number \(i)")
+    }
+  }
+}
+
