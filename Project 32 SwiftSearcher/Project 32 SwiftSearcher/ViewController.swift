32b86333cfeac2ab147d46be17189525f3cecba4
Felix <linfelix165@gmail.com>
2018-07-31 16:24:58 -0400

    Project 32 Init

---
+++ Project 32 SwiftSearcher/Project 32 SwiftSearcher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project 32 SwiftSearcher
+//
+//  Created by Felix Lin on 7/31/18.
+//  Copyright © 2018 Felix Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
