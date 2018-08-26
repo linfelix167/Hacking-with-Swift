016bd834e105cdca02b6bb00f0c9d1e113062c01
Felix <linfelix165@gmail.com>
2018-08-25 23:36:17 -0600

    Project 39 Init

---
+++ Project 39 Unit testing with XCTest/Project 39 Unit testing with XCTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 39 Unit testing with XCTest
+//
+//  Created by Felix Lin on 8/25/18.
+//  Copyright © 2018 Felix Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ Project 39 Unit testing with XCTest/Project 39 Unit testing with XCTestTests/Project_39_Unit_testing_with_XCTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  Project_39_Unit_testing_with_XCTestTests.swift
+//  Project 39 Unit testing with XCTestTests
+//
+//  Created by Felix Lin on 8/25/18.
+//  Copyright © 2018 Felix Lin. All rights reserved.
+//
+
+import XCTest
+@testable import Project_39_Unit_testing_with_XCTest
+
+class Project_39_Unit_testing_with_XCTestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
