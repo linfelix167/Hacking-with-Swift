6c74c3462625b3f406106ec903c00467cd0abd4d
Felix <linfelix165@gmail.com>
2018-09-13 22:54:55 -0400

    Project 39 Done

--- Project 39 Unit testing with XCTest/Project 39 Unit testing with XCTestTests/Project_39_Unit_testing_with_XCTestTests.swift
+++ Project 39 Unit testing with XCTest/Project 39 Unit testing with XCTestTests/Project_39_Unit_testing_with_XCTestTests.swift
@@ -10,25 +10,58 @@ import XCTest
 @testable import Project_39_Unit_testing_with_XCTest
 
 class Project_39_Unit_testing_with_XCTestTests: XCTestCase {
-
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testAllWordsLoaded() {
+        let playData = PlayData()
+        XCTAssertEqual(playData.allWords.count, 18440, "allWords must be 18440")
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testWordCountsAreCorrect() {
+        let playData = PlayData()
+        XCTAssertEqual(playData.wordCounts.count(for: "home"), 174, "Home does not appear 174 times")
+        XCTAssertEqual(playData.wordCounts.count(for: "fun"), 4, "Fun does not appear 4 times")
+        XCTAssertEqual(playData.wordCounts.count(for: "mortal"), 41, "Mortal does not appear 41 times")
+    }
+    
+    func testWordsLoadQuickly() {
+        measure {
+            _ = PlayData()
         }
     }
-
+    
+    func testUserFilterWorks() {
+        let playData = PlayData()
+        
+        playData.applyUserFilter("100")
+        
+        XCTAssertEqual(playData.filteredWords.count, 495)
+        
+        playData.applyUserFilter("1000")
+        
+        XCTAssertEqual(playData.filteredWords.count, 55)
+        
+        playData.applyUserFilter("10000")
+        
+        XCTAssertEqual(playData.filteredWords.count, 1)
+        
+        playData.applyUserFilter("test")
+        
+        XCTAssertEqual(playData.filteredWords.count, 56)
+        
+        playData.applyUserFilter("swift")
+        
+        XCTAssertEqual(playData.filteredWords.count, 7)
+        
+        playData.applyUserFilter("objective-c")
+        
+        XCTAssertEqual(playData.filteredWords.count, 0)
+    }
 }
